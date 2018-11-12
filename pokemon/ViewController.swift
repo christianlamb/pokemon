f52fa3f908588c0d37e6ad66be104bb4f3ea2501
Christian Lamb <christianlamb@MacBook-Pro-52.local>
2018-11-12 10:37:05 -0600

    Initial Commit

---
+++ pokemon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokemon
+//
+//  Created by Christian Lamb on 11/12/18.
+//  Copyright © 2018 Christian Lamb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
